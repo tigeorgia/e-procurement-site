980010f09c5e7cd932efb92f8afd05536eb5bc60
Chris <chris@transparency.ge>
2013-06-12 15:11:39 +0400

    fixed email links

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,7 +35,7 @@ BootstrapStarter::Application.configure do
   config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.default_url_options = {
-    :host => '0.0.0.0',
+    :host => "0.0.0.0",
     :port => 3000,
     :locale => "en"
   }
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,7 +33,9 @@ BootstrapStarter::Application.configure do
 	config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
 
-  config.action_mailer.default_url_options[:host] = "tenderwatch.tigeorgia.webfactional.com",
-  config.action.mailer.default_url_options[:locale] = "en"
+  config.action_mailer.default_url_options = {
+    :host => "tenderwatch.tigeorgia.webfactional.com"
+    :locale => "en"
+  }
     
 end
