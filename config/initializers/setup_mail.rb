f23642302ea61a73c642846c8e992185d0609c00
Chris <chris@transparency.ge>
2013-06-12 15:27:06 +0400

    updated settings

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -3,7 +3,7 @@ ActionMailer::Base.smtp_settings = {
   :port                 => 587,
   :domain               => "gmail.com",
   :user_name            => "procwatch",
-  :password             => "thisisthemailpassword",
+  :password             => "tismp",
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
