4749b904282180c5083c5d4781380df2d8262013
Chris <chris@transparency.ge>
2013-06-12 14:45:49 +0400

    attempt to fix production emailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,7 +7,7 @@ BootstrapStarter::Application.configure do
   config.cache_classes = true
 
   # Log error messages when you accidentally call methods on nil.
-  config.whiny_nils = false
+  config.whiny_nils = true
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = false
@@ -15,6 +15,8 @@ BootstrapStarter::Application.configure do
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
+
+  config.active_support.deprecation = :log
   
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
@@ -31,8 +33,7 @@ BootstrapStarter::Application.configure do
 	config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
 
-  config.action_mailer.default_url_options = {
-    :host => "tenderwatch.tigeorgia.webfactional.com",
-    :locale => "en"
-  }
+  config.action_mailer.default_url_options[:host] = "tenderwatch.tigeorgia.webfactional.com",
+  config.action.mailer.default_url_options[:locale] = "en"
+    
 end
