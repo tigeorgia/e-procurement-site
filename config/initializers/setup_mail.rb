44d59e978d352896bbc9d0246cdb6b296011b6a1
Chris <chris@transparency.ge>
2013-07-04 15:27:03 +0400

    removed uneeded file

--- config/initializers/setup_mail.rb
+++
@@ -1,9 +0,0 @@
-ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.webfaction.com",
-  :port                 => 587,
-  :domain               => "tendermonitor.ge",
-  :user_name            => "tendermonitor",
-  :password             => "k~C4S]F!R>onc>g",
-  :authentication	=> "plain",
-  :enable_starttls_auto => true
-}
