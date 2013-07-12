2f17baa736a22ebc86c798bd4a880ba3c49dae9a
Chris <chris@transparency.ge>
2013-07-12 10:39:14 +0400

    fixed crash when creating cpv tree string

--- lib/graph_helper.rb
+++ lib/graph_helper.rb
@@ -1,6 +1,6 @@
 module GraphHelper
   
-  def createTreeGraphStringFromAgreements( agreements )
+  def self.createTreeGraphStringFromAgreements( agreements )
     cpvTree = []
     done = false
     while not done
@@ -9,7 +9,7 @@ module GraphHelper
         if key == "99999999"
           next
         end     
-        parentKey = getParentCode(key)
+        parentKey = self.getParentCode(key)
         if parentKey and not agreements[parentKey]
           parent = TenderCpvClassifier.where(:cpv_code => parentKey).first
           if parent
@@ -38,17 +38,17 @@ module GraphHelper
       #lets make a tree out of our CPV codes
       root = { :name => "", :code => "00000000", :children => [] }
       cpvTree.sort! {|x,y| x[:code] <=> y[:code] }
-      root = createTree( root, cpvTree )
-      root = createUndefinedCategories( root )
-      calcParentVal( root )
-      jsonString = createJsonString( root, jsonString )
+      root = self.createTree( root, cpvTree )
+      root = self.createUndefinedCategories( root )
+      self.calcParentVal( root )
+      jsonString = self.createJsonString( root, jsonString )
       jsonString.chop!
     end
     return jsonString
   end
 
-  def getParentCode( code )
-    digits = countZeros(code)
+  def self.getParentCode( code )
+    digits = self.countZeros(code)
     parentSuffix = ""
     if digits < 6 
       parentPrefix = code[0, code.length-(digits+1)]
@@ -60,12 +60,12 @@ module GraphHelper
     return nil 
   end  
 
-  def createTree( root, list )
+  def self.createTree( root, list )
     prev = root
 
     list.each do |node|
       parent = prev
-      while not isChild(parent,node)
+      while not self.isChild(parent,node)
         parent = parent[:parent]
       end
        
@@ -76,7 +76,7 @@ module GraphHelper
     return root
   end
 
-  def countZeros( string )
+  def self.countZeros( string )
     count = 0
     pos = string.length
     while pos > 0
@@ -90,18 +90,18 @@ module GraphHelper
     return count
   end
 
-  def isChild(parent, node)
+  def self.isChild(parent, node)
     if parent[:code] == "00000000" 
       return true
     elsif parent[:code] == node[:code]
       return false
     else
-      nodeDigits = countZeros(node[:code])
+      nodeDigits = self.countZeros(node[:code])
       #6 zeros or more means this is a primary grouping
       if nodeDigits >= 6
         return false
       end
-      digits = countZeros(parent[:code])
+      digits = self.countZeros(parent[:code])
       parentString = parent[:code]
       subParent = parentString[0, parentString.length-digits]
       codeString = node[:code]
@@ -112,19 +112,19 @@ module GraphHelper
 
 
   #make parent values the sum of all child values
-  def calcParentVal( root )
+  def self.calcParentVal( root )
     if root[:children].length == 0
       return root[:value]
     else
       root[:children].each do |child|
-        root[:value] += calcParentVal( child )
+        root[:value] += self.calcParentVal( child )
       end
       return root[:value]
     end
   end
 
   #pass parent category values down into new uncategorised childs
-  def createUndefinedCategories( root )
+  def self.createUndefinedCategories( root )
     if not root[:value]
       root[:value] = 0
     end
@@ -136,13 +136,13 @@ module GraphHelper
       end
       root[:value] = 0
       root[:children].each do |child|
-        createUndefinedCategories(child)
+        self.createUndefinedCategories(child)
       end
     end
     return root
   end
 
-  def createJsonString( root, jsonString )
+  def self.createJsonString( root, jsonString )
     jsonString +="{"
     jsonString += '"name": ' + '"'+root[:name]+'"'+','
     jsonString += '"value": ' + root[:value].to_s+','
@@ -151,7 +151,7 @@ module GraphHelper
     if root[:children].length > 0
       jsonString += '"children": ['
       root[:children].each do |child|
-        jsonString += createJsonString( child, "" )
+        jsonString += self.createJsonString( child, "" )
       end
       jsonString += ']'
     end
